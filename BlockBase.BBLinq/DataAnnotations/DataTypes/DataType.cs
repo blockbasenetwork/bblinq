e4d822061dfa7f790e45a612715c61d0e8bf82ae
Fabio <fabio_jesus@live.com>
2020-11-29 14:13:00 +0000

    changing base. start from here.

--- BlockBase.BBLinq/DataAnnotations/DataTypes/DataType.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace BlockBase.BBLinq.DataAnnotations.DataTypes
-{
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
-    public abstract class DataTypeAttribute : Attribute
-    {
-    }
-}
